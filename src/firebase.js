5bf40ff2f5dfba8aba09deaa4bd0d146dfd06fec
MakarukArsen <makarukarsen@gmail.com>
2022-12-25 14:17:13 +0200

    fixed error

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_DISCOUNTS_API_KEY,
-    authDomain: process.env.REACT_APP_DISCOUNTS_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_DISCOUNTS_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_DISCOUNTS_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_DISCOUNTS_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_DISCOUNTS_APP_ID,
+    apiKey: process.env.REACT_APP_DS_API_KEY,
+    authDomain: process.env.REACT_APP_DS_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_DS_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_DS_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_DS_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_DS_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
